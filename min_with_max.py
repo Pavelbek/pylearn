d7a055213830abcaf6659da426988a54cd9e2fe8
Pavel <pavel.argunov@mediasapiens.co>
2015-11-12 11:11:56 +0200

    MAde code closer to task from Checkio

--- min_with_max.py
+++ min_with_max.py
@@ -1,6 +1,16 @@
-n = 0
+num_list = [-4, -5, 5, 4.2, 5.1]
 
-for i in range(-5,5):
-    n += i
+def min_max(numbers):
+
+    min = 0
+    max = 0
+
+    for i in numbers:
+        if i < min:
+            min = i
+        elif i > max:
+            max = i
+    return abs(min) + abs(max)
     
-print(n)
+print(min_max(num_list))
+    
