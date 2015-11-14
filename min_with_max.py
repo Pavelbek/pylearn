9d1f0505e78c4202ea0444673753ca1a3e4f75f9
Pavel Argunov <paularg1807@gmail.com>
2015-11-14 15:12:51 +0200

    Finishe one more task from Checkio

--- min_with_max.py
+++ min_with_max.py
@@ -1,7 +1,12 @@
+
+# Function returns odds between the biggest and the smallest numbers in a list
+
+# This is my solution, may be it is not so good
+
 num_list = [-4, -5, 5, 4.2, 5.1]
 
 def min_max(numbers):
-    if not len(num_list):
+    if not len(numbers):
         return 0
     min = num_list[0]
     max = num_list[0]
@@ -14,4 +19,13 @@ def min_max(numbers):
     return max - min
     
 print(min_max(num_list))
+
+
+# This is better solution taken from Checkio
+
+def checkio(*args):
+    return max(args) - min(args) if args else 0
+	
+	
+
     
