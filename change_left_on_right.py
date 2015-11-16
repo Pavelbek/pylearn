3016a9d1beac5cfd950d0381d82b98f92d88059d
Pavel Argunov <paularg1807@gmail.com>
2015-11-16 21:24:30 +0200

    solved task 27 from Checkio

--- change_left_on_right.py
+++ change_left_on_right.py
@@ -1,14 +1,31 @@
+
+# Here we need to change all "right" words in string to word "left"
+# We created separator "," and join sequence separated by our separator
+# Finally we checked if the word exist in sequence change it on other word and return string
+
+
 first_tuple = ("left", "right", "left", "stop")
 
 def left_join(phrases):
     """
         Join strings and replace "right" to "left"
     """
-    string = ""
+    sequence = []
+    s = ","
     for word in phrases:
-        string += word + ","
+        sequence.append(word)
+    string = s.join(sequence)
     if "right" in string:
         return string.replace("right", "left")
     return string
     
 print(left_join(first_tuple))
+
+
+# Not mine solution but better than mine. 
+
+def left_join(phrases):
+    """
+        Join strings and replace "right" to "left"
+    """
+    return (",".join(phrases)).replace("right","left")
